1ad76f62b34ded4eb7575f47d348a59b56ef262f
Nikhil <kumarnikhil787031@gmail.com>
2023-08-27 12:49:33 +0530

    StopWatch added in this clock

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ let sec = document.getElementById('sec');
 let dayDate = document.getElementById('dayDate');
 let tickSound = new Audio('tick.mp3');
 
+
 // Function to convert number to words (e.g., 12 to "twelve")
 function numberToWords(num) {
     // Add your number to words conversion logic here
@@ -60,3 +61,39 @@ setInterval(displayTime, 1000);
 // Event listener for the "Speak Time" button
 let speakButton = document.getElementById('speakTimeButton');
 speakButton.addEventListener('click', speakTime);
+
+let stopwatchButton = document.getElementById('startStopwatchButton');
+let stopwatchDisplay = document.getElementById('stopwatch');
+let isStopwatchRunning = false;
+let stopwatchInterval;
+let stopwatchStartTime = 0; // Initialize the starting time to 0
+let lastRecordedTime = 0; // Store the last recorded time
+
+function formatTime(time) {
+    let hours = Math.floor(time / 3600);
+    let minutes = Math.floor((time % 3600) / 60);
+    let seconds = time % 60;
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
+function updateStopwatchDisplay() {
+    let currentTime = lastRecordedTime + Math.floor((Date.now() - stopwatchStartTime) / 1000);
+    stopwatchDisplay.textContent = formatTime(currentTime);
+}
+
+function toggleStopwatch() {
+    if (isStopwatchRunning) {
+        clearInterval(stopwatchInterval);
+        stopwatchButton.textContent = 'Start Stopwatch';
+        lastRecordedTime += Math.floor((Date.now() - stopwatchStartTime) / 1000); // Store the last recorded time
+    } else {
+        stopwatchStartTime = Date.now();
+        stopwatchInterval = setInterval(updateStopwatchDisplay, 1000);
+        stopwatchButton.textContent = 'Stop Stopwatch';
+    }
+    isStopwatchRunning = !isStopwatchRunning;
+}
+
+stopwatchButton.addEventListener('click', toggleStopwatch);
+
+
